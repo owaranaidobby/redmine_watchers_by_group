8abf630593bad0498c86998528c55e51f4714245
shimomura <tatshimomura@gmail.com>
2012-05-22 13:12:03 +0900

    new以外にshowページ(=チケット更新時)でもwatcherのチェックボックスとselectUIが出るように修正

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -1,11 +1,8 @@
 module Redmine_watchers_by_group
   class Hooks < Redmine::Hook::ViewListener
     def view_issues_form_details_bottom(context={ })
-      controller = context[:controller]
-      return '' unless controller
-      action_name = controller.action_name
-      return '' unless action_name
-      return '' unless action_name=="new"
+      context[:watchers] = (context[:issue].project.users.sort + context[:issue].watcher_users).uniq
+
       context[:controller].send(:render_to_string, {
           :partial => 'watchers/multiselect_group',
           :locals => context
