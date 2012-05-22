7daff39f845289357304b5cc68dd9e73c478b5cd
shimomura <tatshimomura@gmail.com>
2012-05-22 13:17:56 +0900

    更新時でもwatcher追加してくれるhookメソッド追加

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -1,5 +1,14 @@
 module Redmine_watchers_by_group
   class Hooks < Redmine::Hook::ViewListener
+    def controller_issues_edit_before_save(context)
+      issue = context[:issue]
+      context[:params][:issue][:watcher_user_ids].each do |watcher_id|
+        watcher = User.find(watcher_id)
+        if issue.addable_watcher_users.include?(watcher)
+          issue.add_watcher(watcher)
+        end
+      end
+    end
     def view_issues_form_details_bottom(context={ })
       context[:watchers] = (context[:issue].project.users.sort + context[:issue].watcher_users).uniq
 
