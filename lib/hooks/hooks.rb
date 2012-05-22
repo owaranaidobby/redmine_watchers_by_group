d558710682c2e346987e938c45521547b2ab65a1
shimomura <tatshimomura@gmail.com>
2012-05-22 15:20:10 +0900

    チェックを外した場合にwatcher登録解除されない不具合修正

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -2,13 +2,21 @@ module Redmine_watchers_by_group
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_edit_before_save(context)
       issue = context[:issue]
-      context[:params][:issue][:watcher_user_ids].each do |watcher_id|
-        watcher = User.find(watcher_id)
-        if issue.addable_watcher_users.include?(watcher)
-          issue.add_watcher(watcher)
-        end
+      current_watcher_id = issue.watchers.map{|watcher| watcher.user_id}
+      new_watcher_id = context[:params][:issue][:watcher_user_ids].map{|watcher| watcher.to_i}
+
+      remove_watcher = User.find(current_watcher_id - new_watcher_id)
+      add_watcher = User.find(new_watcher_id - current_watcher_id)
+
+      remove_watcher.each do |watcher|
+        issue.remove_watcher(watcher)
+      end
+
+      add_watcher.each do |watcher|
+        issue.add_watcher(watcher)
       end
     end
+
     def view_issues_form_details_bottom(context={ })
       context[:watchers] = (context[:issue].project.users.sort + context[:issue].watcher_users).uniq
 
