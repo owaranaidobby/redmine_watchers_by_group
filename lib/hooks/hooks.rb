79e650642f91db086679bba42b5190213455b270
shimomura <tatshimomura@gmail.com>
2012-05-25 20:20:59 +0900

    ウォッチャーを一人も選んでない場合に更新しようとするとエラーが出るのを修正

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -2,8 +2,9 @@ module Redmine_watchers_by_group
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_edit_before_save(context)
       issue = context[:issue]
-      current_watcher_id = issue.watchers.map{|watcher| watcher.user_id}
-      new_watcher_id = context[:params][:issue][:watcher_user_ids].map{|watcher| watcher.to_i}
+      current_watcher_id = issue.watchers ? issue.watchers.map{|watcher| watcher.user_id} : []
+      new_watchers = context[:params][:issue][:watcher_user_ids]
+      new_watcher_id = new_watchers ? new_watchers.map{|watcher| watcher.to_i} : []
 
       remove_watcher = User.find(current_watcher_id - new_watcher_id)
       add_watcher = User.find(new_watcher_id - current_watcher_id)
