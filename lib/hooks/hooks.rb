b60d0f1c7371ab523e49a95548ec9106afe19931
owaranaidobby <owaranaiutawoutaoujp@gmail.com>
2014-04-07 12:19:29 +0900

    グループが存在しない場合、チケット更新時にウォッチャーが全削除されるのを修正

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -3,18 +3,20 @@ module Redmine_watchers_by_group
     def controller_issues_edit_before_save(context)
       issue = context[:issue]
       current_watcher_id = issue.watchers ? issue.watchers.map{|watcher| watcher.user_id} : []
-      new_watchers = context[:params][:issue][:watcher_user_ids]
-      new_watcher_id = new_watchers ? new_watchers.map{|watcher| watcher.to_i} : []
+      if context[:params][:issue].key?(:watcher_user_ids)
+        new_watchers = context[:params][:issue][:watcher_user_ids]
+        new_watcher_id = new_watchers ? new_watchers.map{|watcher| watcher.to_i} : []
 
-      remove_watcher = User.find(current_watcher_id - new_watcher_id)
-      add_watcher = User.find(new_watcher_id - current_watcher_id)
+        remove_watcher = User.find(current_watcher_id - new_watcher_id)
+        add_watcher = User.find(new_watcher_id - current_watcher_id)
 
-      remove_watcher.each do |watcher|
-        issue.remove_watcher(watcher)
-      end
+        remove_watcher.each do |watcher|
+          issue.remove_watcher(watcher)
+        end
 
-      add_watcher.each do |watcher|
-        issue.add_watcher(watcher)
+        add_watcher.each do |watcher|
+          issue.add_watcher(watcher)
+        end
       end
     end
 
